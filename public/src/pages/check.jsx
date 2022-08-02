a2a7ee1fac2f1a079179991e1109121775eb5bc9
Abhishek Sharma <abhishekshrm0014@gmail.com>
2022-08-02 14:50:21 +0530

    first commit

---
+++ public/src/pages/check.jsx
@@ -0,0 +1,10 @@
+export default function myName(){
+    return(
+        <div>
+            <h1>
+                asdhkasd
+            </h1>
+            <a href="mailto:12121212">hi whts au=up</a>
+        </div>
+    )
+}
